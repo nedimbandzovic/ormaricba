502876dbbd7ba2aa857ca4561ab0c6f3e87a2c7b
Nedim Bandžović <nedim.bandzovic2001@gmail.com>
2022-05-18 13:20:46 +0200

    Add exit button logic

--- app/src/main/java/com/example/ormaricba/HomeActivity.java
+++ app/src/main/java/com/example/ormaricba/HomeActivity.java
@@ -7,11 +7,13 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class HomeActivity extends AppCompatActivity {
 
     Button button1,button2;
+    ImageView close;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +21,7 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main2);
         getSupportActionBar().hide();
         button1=findViewById(R.id.button1);
+        close=findViewById(R.id.imageView5);
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -34,6 +37,16 @@ public class HomeActivity extends AppCompatActivity {
                 Intent intent2=new Intent(HomeActivity.this, RegisterActivity.class);
                 startActivity(intent2);
                 overridePendingTransition(0,0);
+            }
+        });
+        close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent homeIntent = new Intent(Intent.ACTION_MAIN);
+                homeIntent.addCategory( Intent.CATEGORY_HOME );
+                homeIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(homeIntent);
+                System.exit(1);
             }
         });
 
