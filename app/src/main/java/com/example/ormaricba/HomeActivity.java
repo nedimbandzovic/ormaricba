7fc0942df8e2847b8ef7b64e98334d201fbf861b
Nedim Bandžović <nedim.bandzovic2001@gmail.com>
2022-05-18 11:54:46 +0200

    Add login screen

--- app/src/main/java/com/example/ormaricba/HomeActivity.java
+++ app/src/main/java/com/example/ormaricba/HomeActivity.java
@@ -2,16 +2,32 @@ package com.example.ormaricba;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class HomeActivity extends AppCompatActivity {
+
+    Button button1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         getSupportActionBar().hide();
+        button1=findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent (HomeActivity.this, LoginActivity.class) ;
+            startActivity(intent);
+                overridePendingTransition(0, 0);
+                ;}
+        });
+
 
 
     }
---
+++ app/src/main/java/com/example/ormaricba/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.example.ormaricba;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        getSupportActionBar().hide();
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
